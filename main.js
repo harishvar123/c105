662b24d830f1a70f913629961763ee41a341f9c1
harishvar123 <85819500+harishvar123@users.noreply.github.com>
2021-08-11 17:51:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+Webcam.set({
+    height:350,
+    width:300,
+    image_format:'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+
+    })
+}
+console.log('ml5 version:', ml5.version);
+classifer=ml5.image_classifer('https://teachablemachine.withgoogle.com/models/M1SP0aGLC/modal.jason',modalLoaded);
+
+function modalLoaded()
+{
+    console.log('Modal Loaded!');
+}
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+    }
+    function gotResult(error, results)
+    {
+        if(error){
+            console.error(error);
+        } else{
+            console.log(results);
+            document.getElementById("result_object_name").innerHTML = results[0].label;
+            document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+                   }
+        }
+    
